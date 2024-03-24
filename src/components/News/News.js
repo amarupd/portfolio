9ed1a7b24b3d7829e5b9717edb039812d0a3c93a
mymaster11tech <mymaster11tech@gmail.com>
2024-03-24 16:17:17 +0530

    bug fix

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,32 +1,38 @@
 // import axios from "axios";
 // import React, { useEffect, useState } from "react";
-// import { Col, Container, Row } from "react-bootstrap";
+// import { Col, Container, Dropdown, DropdownButton, Row } from "react-bootstrap";
 // import Particle from "../Particle";
-// import NewsCard from "./NewsCards"; // Assuming you have NewsCard component
+// import NewsCard from "./NewsCards";
 
 // function News() {
 //   const [articles, setArticles] = useState([]);
+//   const [selectedCategory, setSelectedCategory] = useState('technology'); // Default category
 
 //   useEffect(() => {
 //     const fetchArticles = async () => {
 //       try {
 //         const response = await axios.get(
-//           "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=46d1a7fbcf014692824e2d86774c6839"
+//           `https://newsapi.org/v2/top-headlines?country=in&category=${selectedCategory}&apiKey=46d1a7fbcf014692824e2d86774c6839`
 //         );
-//         console.log(response.data.articles);
 //         const filteredArticles = response.data.articles.map(article => ({
 //             title: article.title,
 //             imgPath: article.urlToImage,
-//             description: article.description ? `${article.description} ${article.content || ''}` : article.content || article.title
+//             description: article.description ? `${article.description} ${article.content || ''}` : article.content || article.title,
+//             url: article.url
 //         }));
-//           setArticles(filteredArticles);
+//         setArticles(filteredArticles);
+//         console.log(filteredArticles);
 //       } catch (error) {
 //         console.error("Error fetching articles:", error);
 //       }
 //     };
 
 //     fetchArticles();
-//   }, []);
+//   }, [selectedCategory]);
+
+//   const handleCategoryChange = (category) => {
+//     setSelectedCategory(category);
+//   };
 
 //   return (
 //     <Container fluid className="project-section">
@@ -38,6 +44,15 @@
 //         <p style={{ color: "white" }}>
 //           Here are some of latest news posts.
 //         </p>
+//         <DropdownButton title="News Category" onSelect={handleCategoryChange}>
+//           <Dropdown.Item eventKey="business">Business</Dropdown.Item>
+//           <Dropdown.Item eventKey="entertainment">Entertainment</Dropdown.Item>
+//           <Dropdown.Item eventKey="general">General</Dropdown.Item>
+//           <Dropdown.Item eventKey="health">Health</Dropdown.Item>
+//           <Dropdown.Item eventKey="science">Science</Dropdown.Item>
+//           <Dropdown.Item eventKey="sports">Sports</Dropdown.Item>
+//           <Dropdown.Item eventKey="technology">Technology</Dropdown.Item>
+//         </DropdownButton>
 //         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
 //           {articles.map((article, index) => (
 //             <Col md={4} className="project-card" key={index}>
@@ -46,6 +61,7 @@
 //                 isBlog={false}
 //                 title={<span className="purple">{article.title}</span>}
 //                 description={article.description}
+//                 ghLink={article.url}
 //               />
 //             </Col>
 //           ))}
@@ -55,10 +71,13 @@
 //   );
 // }
 
-import axios from "axios";
+// export default News;
+
+
 import React, { useEffect, useState } from "react";
 import { Col, Container, Dropdown, DropdownButton, Row } from "react-bootstrap";
 import Particle from "../Particle";
+import articlesData from "./Articles.json"; // Importing the JSON data
 import NewsCard from "./NewsCards";
 
 function News() {
@@ -66,22 +85,10 @@ function News() {
   const [selectedCategory, setSelectedCategory] = useState('technology'); // Default category
 
   useEffect(() => {
-    const fetchArticles = async () => {
-      try {
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=in&category=${selectedCategory}&apiKey=46d1a7fbcf014692824e2d86774c6839`
-        );
-        const filteredArticles = response.data.articles.map(article => ({
-            title: article.title,
-            imgPath: article.urlToImage,
-            description: article.description ? `${article.description} ${article.content || ''}` : article.content || article.title,
-            url: article.url
-        }));
-        setArticles(filteredArticles);
-        console.log(filteredArticles);
-      } catch (error) {
-        console.error("Error fetching articles:", error);
-      }
+    const fetchArticles = () => {
+      // Filter articles based on selected category
+      const filteredArticles = articlesData.filter(article => article.category === selectedCategory);
+      setArticles(filteredArticles);
     };
 
     fetchArticles();
@@ -114,7 +121,7 @@ function News() {
           {articles.map((article, index) => (
             <Col md={4} className="project-card" key={index}>
               <NewsCard
-                imgPath={article.imgPath}
+                imgPath={article.urlToImage}
                 isBlog={false}
                 title={<span className="purple">{article.title}</span>}
                 description={article.description}
